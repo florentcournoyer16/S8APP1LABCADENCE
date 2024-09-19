624e53b6c5eaa0d09961a9bc30ca322a4180aee2
Florent <couf1001@usherbrooke.ca>
2024-09-19 16:18:26 -0400

    LAB 3 DONE

--- LAB3/fifo_property.sv
+++ LAB3/fifo_property.sv
@@ -24,91 +24,105 @@ sequence default_values;
   fifo_full == 1'b0
 endsequence
 
-property check_reset;
+property prop_check_reset;
   @(posedge clk) !rst_ |-> ##0 default_values;
 endproperty
 
-property check_reset_;
+property prop_check_reset_;
   @(posedge clk) !rst_ ##0 default_values;
 endproperty
 
-ass_check_reset: assert property (check_reset) else $display($stime,"\t\t FAIL::check_reset\n");
-cov_check_reset: cover property (check_reset_) $display($stime,"\t\t PASS::check_reset\n");
+ass_check_reset: assert property (prop_check_reset) else $display($stime,"\t\t FAIL::check_reset\n");
+cov_check_reset: cover property (prop_check_reset_) $display($stime,"\t\t PASS::check_reset\n");
 
 // ------------------------------------------
 // 2. Check that fifo_empty is asserted the same clock that fifo 'cnt' is 0. Disable this property 'iff (!rst)'
 // ------------------------------------------
 
-
-property fifo_empty_when_cnt_0;
-  @(posedge clk) disable iff(!rst_) `cnt == 8'b00000000 |-> ##0 fifo_empty;
+property prop_fifo_empty_when_cnt_0;
+  @(posedge clk) disable iff(!rst_) (`cnt == 8'b00000000) |-> ##0 fifo_empty;
 endproperty
 
-property fifo_empty_;
-  @(posedge clk) disable iff(!rst_) `cnt == 8'b00000000 ##0 fifo_empty;
+property prop_fifo_empty_when_cnt_0_;
+  @(posedge clk) disable iff(!rst_) (`cnt == 8'b00000000) ##0 fifo_empty;
 endproperty
 
-ass_fifo_empty: assert property (fifoempty) else $display($stime,"\t\t FAIL::fifo_empty condition\n");
-cov_fifo_empty: assert property (fifoempty) $display($stime,"\t\t PASS::fifo_empty condition\n");
+ass_fifo_empty_when_cnt_0: assert property (prop_fifo_empty_when_cnt_0) else $display($stime,"\t\t FAIL::fifo_empty_when_cnt_0 condition\n");
+cov_fifo_empty_when_cnt_0: cover property (prop_fifo_empty_when_cnt_0_) $display($stime,"\t\t PASS::fifo_empty_when_cnt_0 condition\n");
 
 
 // ------------------------------------------
 // 3. Check that fifo_full is asserted any time fifo 'cnt' is greater than 7. Disable this property 'iff (!rst)'
-//      ------------------------------------------
+// ------------------------------------------
 
-`ifdef check3
-property fifofull;
-  @(posedge clk) !rst_ |-> `rd_ptr==0; //DUMMY... remove this line and replace it with correct check
-endproperty
-fifofullP: assert property (fifofull) else $display($stime,"\t\t FAIL::fifo_full condition\n");
-`endif
+sequence seq_fifo_saturated;
+  (`cnt > 8'b00000111);
+endsequence
 
-//      ------------------------------------------
-//        4. Check that if fifo is full and you attempt to write (but not read) that
-//           the wr_ptr does not change.
-//      ------------------------------------------
-`ifdef check4
-property fifo_full_write_stable_wrptr;
-  @(posedge clk) !rst_ |-> `rd_ptr==0; //DUMMY... remove this line and replace it with correct check
+property prop_fifo_full_when_cnt_8;
+  @(posedge clk) disable iff(!rst_) seq_fifo_saturated |-> ##0 fifo_full;
 endproperty
-fifo_full_write_stable_wrptrP: assert property (fifo_full_write_stable_wrptr) 
-	else $display($stime,"\t\t FAIL::fifo_full_write_stable_wrptr condition\n");
-`endif
 
-`ifdef check5
-//      ------------------------------------------
-//        5. Check that if fifo is empty and you attempt to read (but not write) that
-//           the rd_ptr does not change.
-//      ------------------------------------------
-property fifo_empty_read_stable_rdptr;
-  @(posedge clk) !rst_ |-> `rd_ptr==0; //DUMMY... remove this line and replace it with correct check
+property prop_fifo_full_when_cnt_8_;
+  @(posedge clk) disable iff(!rst_) seq_fifo_saturated ##0 fifo_full;
 endproperty
-fifo_empty_read_stable_rdptrP: assert property (fifo_empty_read_stable_rdptr) 
-	else $display($stime,"\t\t FAIL::fifo_empty_read_stable_rdptr condition\n");
-`endif
 
-//      ------------------------------------------
-//        6. Write a property to Warn on write to a full fifo
-//	This property will give Warning with all simulations  
-//      ------------------------------------------
-`ifdef check6
-property write_on_full_fifo;
-  @(posedge clk) !rst_ |-> `rd_ptr==0; //DUMMY... remove this line and replace it with correct check
-endproperty
-write_on_full_fifoP: assert property (write_on_full_fifo)
-	else $display($stime,"\t\t WARNING::write_on_full_fifo\n");
-`endif
+ass_fifo_full_when_cnt_8: assert property (prop_fifo_full_when_cnt_8) else $display($stime,"\t\t FAIL::fifo_full_when_cnt_8 condition\n");
+cov_fifo_full_when_cnt_8: cover property (prop_fifo_full_when_cnt_8_) $display($stime,"\t\t PASS::fifo_full_when_cnt_8 condition\n");
 
-//      ------------------------------------------
-//        7. Write a property to Warn on read from an empty fifo
-//	This property will give Warning with all simulations  
-//      ------------------------------------------
-`ifdef check7
-property read_on_empty_fifo;
-  @(posedge clk) !rst_ |-> `rd_ptr==0; //DUMMY... remove this line and replace it with correct check
+// ------------------------------------------
+// 4. Check that if fifo is full and you attempt to write (but not read) that the wr_ptr does not change.
+// ------------------------------------------
+
+property prop_fifo_full_write_stable_wrptr;
+  @(posedge clk) disable iff(!rst_) (seq_fifo_saturated ##0 fifo_write) |-> ##1 $stable(`wr_ptr);
 endproperty
-read_on_empty_fifoP: assert property (read_on_empty_fifo) 
-	else $display($stime,"\t\t WARNING::read_on_empty_fifo condition\n");
-`endif
+
+property prop_fifo_full_write_stable_wrptr_;
+  @(posedge clk) disable iff(!rst_) (seq_fifo_saturated ##0 fifo_write) ##1 $stable(`wr_ptr);
+endproperty
+
+ass_fifo_full_write_stable_wrptr: assert property (prop_fifo_full_write_stable_wrptr)  else $display($stime,"\t\t FAIL::fifo_full_write_stable_wrptr condition\n");
+cov_fifo_full_write_stable_wrptr: cover property (prop_fifo_full_write_stable_wrptr_) $display($stime,"\t\t PASS::fifo_full_write_stable_wrptr condition\n");
+
+// ------------------------------------------
+// 5. Check that if fifo is empty and you attempt to read (but not write) that the rd_ptr does not change.
+// ------------------------------------------
+
+sequence seq_fifo_empty;
+  (`cnt == 8'b00000000);
+endsequence
+
+
+property prop_fifo_empty_read_stable_rdptr;
+  @(posedge clk) disable iff(!rst_) (seq_fifo_empty ##0 fifo_read) |->  ##1 $stable(`rd_ptr);
+endproperty
+
+property prop_fifo_empty_read_stable_rdptr_;
+  @(posedge clk) disable iff(!rst_) (seq_fifo_empty ##0 fifo_read) |->  ##1 $stable(`rd_ptr);
+endproperty
+
+ass_fifo_empty_read_stable_rdptr: assert property (prop_fifo_empty_read_stable_rdptr) else $display($stime,"\t\t FAIL::fifo_empty_read_stable_rdptr condition\n");
+cov_fifo_empty_read_stable_rdptr: cover property (prop_fifo_empty_read_stable_rdptr_) $display($stime,"\t\t PASS::fifo_empty_read_stable_rdptr condition\n");
+
+// ------------------------------------------
+// 6. Write a property to Warn on write to a full fifo This property will give Warning with all simulations  
+// ------------------------------------------
+
+property prop_write_on_full_fifo;
+  @(posedge clk) disable iff(!rst_) seq_fifo_saturated |-> !fifo_write;
+endproperty
+
+// ass_write_on_full_fifo: assert property (prop_write_on_full_fifo) else $display($stime,"\t\t WARNING::write_on_full_fifo\n");
+
+// ------------------------------------------
+// 7. Write a property to Warn on read from an empty fifo. This property will give Warning with all simulations  
+// ------------------------------------------
+
+property prop_read_on_empty_fifo;
+  @(posedge clk) disable iff(!rst_) seq_fifo_empty |-> fifo_read; //DUMMY... remove this line and replace it with correct check
+endproperty
+
+// ASS_read_on_empty_fifo: assert property(prop_read_on_empty_fifo) $display($stime,"\t\t WARNING::read_on_empty_fifo condition\n");
 
 endmodule
